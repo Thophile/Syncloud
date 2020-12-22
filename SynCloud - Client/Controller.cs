3cac56b6b09ad1a7feba5dd7b2b19b951959bb64
Thophile <montemonttheophile@gmail.com>
2020-12-22 19:08:58 +0100

    [Add] Added Singleton controller and Settings enum

---
+++ SynCloud - Client/Controller.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace SynCloud
+{
+    class Controller
+    {
+        private Controller() {
+            //Appdata folder path
+            string appData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SynCloud");
+            Directory.CreateDirectory(appData);
+
+            /* ==== Settings initialisation ==== */
+
+            _settingsFile = Path.Combine(appData, "settings.json");
+
+            if (File.Exists(_settingsFile))
+            {
+                AppSettings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(_settingsFile));
+            }
+            else
+            {
+                AppSettings = new Settings(Settings.Languages.English);
+            }
+        }
+        /* == Private members == */
+        private string _settingsFile;
+        private static Controller _instance = null;
+        private static readonly object _lock = new object();
+
+        /* == Properties = */
+        public Settings AppSettings { get; set; }
+        public static Controller Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Controller();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
---
+++ SynCloud - Client/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SynCloud
+{
+    public struct Settings
+    {
+        public Settings(Languages Language)
+        {
+            this.Language = Language;
+        }
+
+        public Languages Language { get; set; }
+
+        public enum Languages
+        {
+            English,
+            Français
+        }
+    }
+}
