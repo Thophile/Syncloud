34ffb46bc1a0f7754f772afed774e2ead8ae95b3
Thophile <montemonttheophile@gmail.com>
2020-12-24 17:16:12 +0100

    Add, Editing synFolders

--- Syncloud - Client/Pages/Edit.xaml.cs
+++ Syncloud - Client/Pages/Edit.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -19,10 +20,30 @@ namespace Syncloud.Pages
     /// </summary>
     public partial class Edit : Page
     {
+        private SyncFolder _syncFolder;
         public Edit(SyncFolder syncFolder)
         {
             InitializeComponent();
             this.InitializeLanguage();
+
+            _syncFolder = syncFolder;
+            PathField.Text = _syncFolder.Path;
+            NameField.Text = _syncFolder.Name;
+        }
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            _syncFolder.Name = NameField.Text;
+            _syncFolder.Path = PathField.Text;
+            Controller.Instance.UpdateSyncFolders();
+            MainWindow.GetWindow(this).Content = new Home();
+        }
+
+        private void PathBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.SelectedPath = PathField.Text;
+            dialog.ShowDialog();
+            PathField.Text = dialog.SelectedPath;
         }
     }
 }
