c7316002f25d80a9362b1ed55f8a2aece9712ea1
Thophile <montemonttheophile@gmail.com>
2020-12-29 21:35:37 +0100

    Create ApiClient.cs

---
+++ Syncloud - Client/ApiClient.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syncloud
+{
+    public class ApiClient : HttpClient
+    {
+        public string Token { get; set; }
+
+        public ApiClient(string uri)
+        {
+            this.BaseAddress = new Uri(uri);
+        }
+    }
+}
