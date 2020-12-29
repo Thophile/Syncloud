94c856cd5917f9455b4d22a7c2554cf77673cdaa
Thophile <montemonttheophile@gmail.com>
2020-12-29 21:37:31 +0100

    Add, empty portal page for a future web portal for the api

---
+++ Api/pages/portal.php
@@ -0,0 +1,17 @@
+<?php
+if(__FILE__ == $_SERVER['SCRIPT_FILENAME']){
+    include_once $_SERVER['DOCUMENT_ROOT'].'/errors/403.html';
+    die();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Portal
+</body>
+</html>
